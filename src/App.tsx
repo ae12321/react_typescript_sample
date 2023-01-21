949cf392460bba00ad1d24b8c4248940b042a59d
a.endo.w <80916217+ae12321@users.noreply.github.com>
2023-01-22 01:56:18 +0900

    a

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,38 @@ import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  const [name, setName] = useState("marumaru");
-  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) =>
-    setName(event.target.value);
+type CounterTitleProps = { name: string; count: number };
+type CounterProps = { name: string };
 
-  useEffect(() => {
-    console.log(name);
-  }, [name]);
+const CounterTitle = (props: CounterTitleProps) => {
+  return (
+    <h1>
+      {props.name}'s count: {props.count}
+    </h1>
+  );
+};
+const CounterComponent = (props: CounterProps) => {
+  const [count, setCount] = useState(0);
+  const handleAdd = () => setCount((prev) => prev + 1);
+  const handleMinus = () => setCount((prev) => prev - 1);
+  const handleReset = () => setCount(0);
 
   return (
+    <>
+      <CounterTitle name={props.name} count={count}></CounterTitle>
+      <button onClick={handleAdd}>add</button>
+      <button onClick={handleMinus}>minus</button>
+      <button onClick={handleReset}>reset</button>
+    </>
+  );
+};
+
+function App() {
+  return (
     <div className="App">
-      <input type="text" onChange={handleOnChange} />
+      <CounterComponent name="test1" />
+      <CounterComponent name="test2" />
+      <CounterComponent name="test3" />
     </div>
   );
 }
