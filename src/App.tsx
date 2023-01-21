a1b042cabd25cec16d9155af09e419a196deca54
a.endo.w <80916217+ae12321@users.noreply.github.com>
2023-01-22 01:47:37 +0900

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("marumaru");
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setName(event.target.value);
+
+  useEffect(() => {
+    console.log(name);
+  }, [name]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={handleOnChange} />
     </div>
   );
 }
